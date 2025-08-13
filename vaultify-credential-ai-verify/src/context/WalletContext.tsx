40c17eb3a010db6aa852acf1d350a4e4a88aa72c
vipin singh <vipendrarajpoot2@gmail.com>
2025-08-13 23:22:13 +0530

    wallet

--- vaultify-credential-ai-verify/src/context/WalletContext.tsx
+++ vaultify-credential-ai-verify/src/context/WalletContext.tsx
@@ -39,7 +39,7 @@ export const WalletProvider: React.FC<{ children: React.ReactNode }> = ({ childr
       } else {
         toast({
           title: " Wallet Not Found! download the wallet",
-          description: "Please install wallet extension",
+          description: "",
           variant: "destructive",
         });
       }
